e3912c30ea710e94cadcf452d7a1a083b9f1a79f
Nicholas Ferrara <nferrara100@gmail.com>
2022-09-03 18:40:40 +0100

    fix: removed redundant vscode extension

--- install.sh
+++ install.sh
@@ -115,7 +115,6 @@ code --install-extension hbenl.vscode-test-explorer
 code --install-extension littlefoxteam.vscode-python-test-adapter
 code --install-extension markis.code-coverage
 code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-toolsai.jupyter-keymap
 code --install-extension ms-toolsai.jupyter-renderers
