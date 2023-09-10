882cf242f97cdda2bb0d449b5357574ab106c51a
Nicholas Ferrara <nferrara100@gmail.com>
2023-09-10 18:13:00 +0100

    fix: prevent polluting system with old node versions and update npm

--- util/configure_node.sh
+++ util/configure_node.sh
@@ -5,7 +5,14 @@ mkdir -p ~/.nvm
 export NVM_DIR="$HOME/.nvm"
 # shellcheck disable=SC1091
 [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
+
+PREVIOUS_NODE_VERSION=$("nvm version")
 # shellcheck disable=SC1090
 nvm install --lts
+NEW_NODE_VERSION=$("nvm version")
+if [ "$PREVIOUS_NODE_VERSION" != "$NEW_NODE_VERSION" ]; then
+    nvm uinstall "$PREVIOUS_NODE_VERSION"
+fi
 
+npm install -g npm
 npm install -g @githubnext/github-copilot-cli
