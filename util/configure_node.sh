880456c97005d1891609963a456473a91f8ae2c6
Nicholas Ferrara <git@nicholasferrara.net>
2024-05-31 00:15:24 +0100

    fix: syntax error in configure node

--- util/configure_node.sh
+++ util/configure_node.sh
@@ -6,10 +6,10 @@ export NVM_DIR="$HOME/.nvm"
 # shellcheck disable=SC1091
 [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
 
-PREVIOUS_NODE_VERSION=$("nvm version")
+PREVIOUS_NODE_VERSION=$(nvm version)
 # shellcheck disable=SC1090
 nvm install --lts
-NEW_NODE_VERSION=$("nvm version")
+NEW_NODE_VERSION=$(nvm version)
 if [ "$PREVIOUS_NODE_VERSION" != "$NEW_NODE_VERSION" ]; then
     nvm uinstall "$PREVIOUS_NODE_VERSION"
 fi
