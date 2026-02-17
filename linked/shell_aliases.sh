a12af1c707469328e3665f39e455b6b53268ea64
Nicholas Ferrara <git@nicholasferrara.net>
2026-02-17 18:26:06 +0000

    feat: shell line count for any directory

--- linked/shell_aliases.sh
+++ linked/shell_aliases.sh
@@ -10,6 +10,8 @@
 alias please=sudo
 # Count files in current directory and subdirectories
 alias count='find . -type f | wc -l'
+# Count lines in current directory and subdirectories
+alias count_lines='find . -type f -print0 | xargs -0 cat 2>/dev/null | wc -l'
 alias profile.sh="vim ~/.profile.sh"
 alias env.sh="vim ~/.env.sh"
 
